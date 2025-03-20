c7335ceee0294b3f95ce57fa2dd2e2543a20fcba
Jakie Khan <khanjakie651@gmail.com>
2025-03-20 14:13:53 +0545

    changes commited

---
+++ script.js
@@ -0,0 +1,27 @@
+// // // // let myAge=20;
+
+// // // // // let doubleAge=myAge * 2;
+// // // // // console.log(doubleAge);
+
+
+// // // // Challenge 2
+
+// // // let quote="I'm learning Javascript !";
+// // // console.log(JSON.stringify(quote));
+
+// // Basic maths
+
+// function math(a,b)
+// {
+//     return calculate=[a+b,a-b,a*b,a/b]
+// }
+
+// console.log(math(10,5));
+
+function character(a)
+{
+    let b=a.split("")
+    console.log(b[b.length]);
+}
+
+let c=character("Ayush");
