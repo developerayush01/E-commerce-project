b6d64b6f224eac64acd9a39317376f4726380c22
Jakie Khan <khanjakie651@gmail.com>
2025-07-24 13:44:56 +0545

    new

--- productpage.js
+++ productpage.js
@@ -1,21 +1,23 @@
 let quan=document.querySelector(".quantity");
-number
+number=Number(quan.innerHTML);
 let sub=document.querySelector(".minus");
 let add=document.querySelector(".plus");
 
 add.addEventListener("click",()=>{
-    quan.innerHTML=Number(quan.innerHTML)+1;
+    number=Number(quan.innerHTML);
+    quan.innerHTML=number+1;
 })
-
-sub.addEventListener("click",()=>{
-    if(Number(quan.innerHTML)<=0)
-    {
-        alert.log("Hello");
-//     sub.style.color="grey";
-// sub.style.cursor="disabled";
+if(number<=0){
+    sub.style.color="grey";
+sub.style.cursor="disabled";
     }
-else({
-    
+sub.addEventListener("click",()=>{
+    number=Number(quan.innerHTML);
+    if(number<=0){
+    sub.style.color="grey";
+sub.style.cursor="disabled";
+    }
+else{
+    quan.innerHTML=number-1;
 }
-)
 })
