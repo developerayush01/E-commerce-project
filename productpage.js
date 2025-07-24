527a237681962d5087c507b7eb308f81b066b6d1
Jakie Khan <khanjakie651@gmail.com>
2025-07-24 13:25:24 +0545

    New

---
+++ productpage.js
@@ -0,0 +1,21 @@
+let quan=document.querySelector(".quantity");
+number
+let sub=document.querySelector(".minus");
+let add=document.querySelector(".plus");
+
+add.addEventListener("click",()=>{
+    quan.innerHTML=Number(quan.innerHTML)+1;
+})
+
+sub.addEventListener("click",()=>{
+    if(Number(quan.innerHTML)<=0)
+    {
+        alert.log("Hello");
+//     sub.style.color="grey";
+// sub.style.cursor="disabled";
+    }
+else({
+    
+}
+)
+})
